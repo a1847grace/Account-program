3758a513c1eb03709d146877942176afdef2bc9f
a1847grace <a1847grace@gmail.com>
2021-07-18 18:05:51 +0800

    新增讀寫CSV檔案

--- 記帳程式.py
+++ 記帳程式.py
@@ -13,3 +13,7 @@ print(products)
 
 for product in products:
 	print (product[0], '的價格是', product[1], '元')
+
+with open ('products.csv', 'w') as f:
+	for product in products:
+		f.write(product[0] + ',' + product[1] + '\n')
