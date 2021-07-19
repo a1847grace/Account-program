7c8d9f2f801a98a6a13ce40f2b54c013b5e78806
a1847grace <a1847grace@gmail.com>
2021-07-19 15:00:26 +0800

    確認檔案是否存在後讀取

--- 記帳程式.py
+++ 記帳程式.py
@@ -1,13 +1,18 @@
-# 讀取檔案，使用strip刪除/n並使用Split作為逗點切割分句
-# 使用continue直接進行下一回合迴圈
+import os # 載入Operating system
 products =[]
-with open ('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-		print (products)
+if os.path.isfile('products.csv'):
+	print('yeah!有這個檔案')
+	# 讀取檔案，使用strip刪除/n並使用Split作為逗點切割分句
+    # 使用continue直接進行下一回合迴圈
+	with open ('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+			print (products)
+else:
+	print('沒有這個檔案')
 
 #讓使用者輸入
 while True:
@@ -28,7 +33,7 @@ for product in products:
 	print(product[0], '的價格是：', product[1], '元')
 print (products)
 
-
+#寫入檔案
 with open ('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,價格\n')
 	for product in products:
