d486e94231b21d85a01e19cd1368c818e6f60ea3
a1847grace <a1847grace@gmail.com>
2021-07-19 10:51:42 +0800

    增加讀取excel功能

--- 記帳程式.py
+++ 記帳程式.py
@@ -1,4 +1,15 @@
-products = []
+# 讀取檔案，使用strip刪除/n並使用Split作為逗點切割分句
+# 使用continue直接進行下一回合迴圈
+products =[]
+with open ('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, price])
+		print (products)
+
+#讓使用者輸入
 while True:
 	name = input('請輸入商品名稱：')
 	if name == 'q':
@@ -10,10 +21,13 @@ while True:
 	#p.append(price)
 	#p = [name, price]
 	products.append([name,price])
-print(products)
+print(products)	
 
+# 印出所有商品價格
 for product in products:
-	print (product[0], '的價格是', product[1], '元')
+	print(product[0], '的價格是：', product[1], '元')
+print (products)
+
 
 with open ('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,價格\n')
