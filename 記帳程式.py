f6192ce11fd225571b2f24d7f97bfeaa77a013ed
a1847grace <a1847grace@gmail.com>
2021-07-19 10:02:04 +0800

    增加編碼為utf-8

--- 記帳程式.py
+++ 記帳程式.py
@@ -4,6 +4,7 @@ while True:
 	if name == 'q':
 		break
 	price = input ('請輸入商品價格')
+	price = int(price)
 	#p = []
 	#p.append(name)
 	#p.append(price)
@@ -14,6 +15,7 @@ print(products)
 for product in products:
 	print (product[0], '的價格是', product[1], '元')
 
-with open ('products.csv', 'w') as f:
+with open ('products.csv', 'w', encoding='utf-8') as f:
+	f.write('商品,價格\n')
 	for product in products:
-		f.write(product[0] + ',' + product[1] + '\n')
+		f.write(product[0] + ',' + str(product[1]) + '\n')
